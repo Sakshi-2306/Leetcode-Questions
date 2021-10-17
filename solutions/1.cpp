da1cdda3e2fadb3f7a3e6fad1a1b8496bd08bdb2
Sakshi-2306 <91713392+Sakshi-2306@users.noreply.github.com>
2021-10-17 12:26:44 +0530

    Update 1.cpp

--- solutions/1.cpp
+++ solutions/1.cpp
@@ -1,19 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
- public:
-  vector<int> twoSum(vector<int> &arr, int target) {
-    unordered_map<int, int> mp;
-
-    for (int i = 0; i < arr.size(); i++) {
-      int val = target - arr[i];
-      if (mp.find(val) != mp.end()) {
-        return vector<int>{mp[val], i};
-      } else {
-        mp[arr[i]] = i;
-      }
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> arr;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    arr.push_back(i);
+                    arr.push_back(j);
+                    break;
+                    
+                }
+            }
+        }
+        return arr;
+        
     }
-    return {};
-  }
 };
