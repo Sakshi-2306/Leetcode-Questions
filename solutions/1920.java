53a5c1d60f060c4f01cf03de08b02710d276fad7
Sakshi-2306 <91713392+Sakshi-2306@users.noreply.github.com>
2021-10-17 12:19:49 +0530

    Update 1920.java

--- solutions/1920.java
+++ solutions/1920.java
@@ -1,11 +1,13 @@
-import java.util.*;
-
 class Solution {
-  public int[] buildArray(int[] nums) {
-    int ans[] = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      ans[i] = nums[nums[i]];
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans.push_back(nums[nums[i]]);
+        }
+        return ans;
+        
     }
-    return ans;
-  }
-}
+};
+
